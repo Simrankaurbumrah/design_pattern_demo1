3b66f33ce9279e69e3fe49fd7789ea48b84bf287
Simrankaurbumrah <104462059+Simrankaurbumrah@users.noreply.github.com>
2025-08-20 14:25:10 +0530

    Create notifications.groovy

---
+++ abstract_fcatory_pattern/vars/notifications.groovy
@@ -0,0 +1,9 @@
+/**
+* notifications.groovy
+* Notifications helpers (email, slack, etc.)
+*/
+ 
+def sendEmail(String to, String subject, String body) {
+    // Placeholder for sending email
+    echo "Sending email to ${to}: ${subject}"
+}
